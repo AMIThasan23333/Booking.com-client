0c0d22613971f592ae0d072f172603689aecfb8e
My Name <cpgsom819@gmail.com>
2023-04-29 21:15:57 +0800

    google pop up added

--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -63,6 +63,16 @@ const Signup = () => {
       .catch(err => console.log(err))
 
   }
+
+
+  const handleGoogleSignIn = () => {
+
+    signInWithGoogle()
+  }
+
+
+
+
   return (
     <div className='flex justify-center items-center pt-8'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -152,7 +162,9 @@ const Signup = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center space-x-4'>
+        <div 
+        onClick={handleGoogleSignIn }
+        className='flex justify-center space-x-4'>
           <button aria-label='Log in with Google' className='p-3 rounded-sm'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
